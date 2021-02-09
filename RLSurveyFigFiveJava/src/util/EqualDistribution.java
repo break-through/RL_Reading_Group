43bad71766467cd8cb9430b4a3c9c4e078b1bb76
robertvunabandi <rvunabandi@gmail.com>
2021-02-08 22:22:34 -0500

    add an equal distribution class

---
+++ RLSurveyFigFiveJava/src/util/EqualDistribution.java
@@ -0,0 +1,47 @@
+package util;
+
+import java.util.*;
+
+public final class EqualDistribution<S> implements IDistribution<S> {
+    private final List<S> states;
+    private final double prob;
+    private final Random random = new Random();
+    
+     public EqualDistribution(Collection<S> states) {
+         if (states.size() == 0) {
+             throw new IllegalArgumentException(
+                 "EqualDistribution requires at least one element"
+             );
+         }
+         this.states = new ArrayList<>(states);
+         prob = 1 / (double) size();
+     }
+    
+    @Override
+    public double prob(S s) {
+         if (!states.contains(s)) {
+             return 0.0;
+         }
+         return prob;
+    }
+    
+    @Override
+    public S sample() {
+         final int index = random.nextInt(size());
+         return states.get(index);
+    }
+    
+    @Override
+    public List<S> reachables() {
+        return new ArrayList<>(states);
+    }
+    
+    @Override
+    public String toString() {
+        return "EqualDistribution{}";
+    }
+    
+    private int size() {
+         return this.states.size();
+    }
+}
