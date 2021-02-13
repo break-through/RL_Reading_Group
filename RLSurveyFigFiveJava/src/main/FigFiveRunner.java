95f9d7708a06fbc1707c99631abdf26f7fbfcb2d
robertvunabandi <rvunabandi@gmail.com>
2021-02-13 15:04:30 -0500

    update things again

--- RLSurveyFigFiveJava/src/main/FigFiveRunner.java
+++ RLSurveyFigFiveJava/src/main/FigFiveRunner.java
@@ -80,23 +80,30 @@ public class FigFiveRunner {
     }
     
     private static void runFigFive() {
-        final int N = 20;
-        final double ALPHA = 0.01;
+        final int N = 12;
+        final double ALPHA = 0.1;
         final double GAMMA = 0.99;
-        final int K = 4;
-        final int STEPS = 1000;
+        final int K = 8;
+        final int STEPS = 100000;
         FigFiveEnvironment env = new FigFiveEnvironment(N);
         DynaQLearningAgent<IState, IAction> agent = new DynaQLearningAgent<>(env, ALPHA, GAMMA, K);
     
-        System.out.println("ENVIRONMENT STATS:");
         System.out.println("=========================================");
+        System.out.println("Environment:");
         System.out.printf("- getStats: %s\n", env.getStats());
+        System.out.printf("- K: %s\n", K);
+        System.out.printf("- N: %s\n", N);
+        System.out.printf("- ALPHA: %s\n", ALPHA);
+        System.out.printf("- GAMMA: %s\n", GAMMA);
+        System.out.println("Agent:");
+        printAgentDetails(agent);
         if (!agent.canStep()) {
             System.out.println("JUST STARTED BUT CAN'T EVEN STEP! STOPPING NOW.");
             return;
         }
-        System.out.println("Starting State:");
+        System.out.println("Starting Agent State:");
         System.out.println(env.toStringInState(agent.getCurrentState()));
+        System.out.println("BEGIN RUNS");
         for (int i = 0; i < STEPS; i++) {
             System.out.println("=========================================");
             System.out.printf("RUN #%d\n", i);
@@ -107,17 +114,34 @@ public class FigFiveRunner {
             System.out.println("State After Step:");
             System.out.println(env.toStringInState(agent.getCurrentState()));
             System.out.println("Agent Details After Step:");
-            System.out.printf("- getNumSteps: %s \n", agent.getNumSteps());
-            System.out.printf("- getAccumulatedRewards: %s \n", agent.getAccumulatedRewards());
-            System.out.printf("- getNumFullBackups: %s \n", agent.getNumFullBackups());
-            System.out.printf("- getNumSampleBackups: %s \n", agent.getNumSampleBackups());
-            System.out.printf("- getTotalNumBackups: %s \n", agent.getTotalNumBackups());
+            printAgentDetails(agent);
             if (!agent.canStep()) {
+                System.out.println();
                 System.out.println("STOPPING BECAUSE CAN'T STEP ANYMORE");
                 break;
             }
             System.out.println();
         }
         System.out.println();
+        System.out.println("OK DONE. Here are details one more time");
+        System.out.println("=========================================");
+        System.out.println("Environment:");
+        System.out.printf("- getStats: %s\n", env.getStats());
+        System.out.printf("- K: %s\n", K);
+        System.out.printf("- N: %s\n", N);
+        System.out.printf("- ALPHA: %s\n", ALPHA);
+        System.out.printf("- GAMMA: %s\n", GAMMA);
+        System.out.println("Agent:");
+        System.out.printf("- lastExperience: %s \n", agent.lastExperience());
+        printAgentDetails(agent);
+    }
+    
+    private static void printAgentDetails(DynaQLearningAgent<IState, IAction> agent) {
+        System.out.printf("- getNumSteps: %s \n", agent.getNumSteps());
+        System.out.printf("- getAccumulatedRewards: %s \n", agent.getAccumulatedRewards());
+        System.out.printf("- getNumFullBackups: %s \n", agent.getNumFullBackups());
+        System.out.printf("- getNumSampleBackups: %s \n", agent.getNumSampleBackups());
+        System.out.printf("- getTotalNumBackups: %s \n", agent.getTotalNumBackups());
+        System.out.printf("- getAlpha: %s \n", agent.getAlpha());
     }
 }
