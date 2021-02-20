c2b98bef899f0720c2e61c342828ecd5ae726572
Victor Sindato <victor@Victors-MacBook-Pro.local>
2021-02-20 08:26:19 -0500

    Add the 'add' abstract method to IDistribution

--- RLSurveyFigFiveJava/src/util/IDistribution.java
+++ RLSurveyFigFiveJava/src/util/IDistribution.java
@@ -5,7 +5,7 @@ import java.util.List;
 public interface IDistribution<S> {
     double prob(S s);
     S sample();
-    
+    void add(S s);
     // Returns a list of states such that this.prob(s) > 0 for all s
     // in that list
     List<S> reachables();
